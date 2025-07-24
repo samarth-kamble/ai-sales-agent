d42495e36359e662bcd5f82c4818a58e8d32bca6
Samarth Kamble <kamblesamarth04@gmail.com>
2025-07-24 23:19:14 +0530

    feat: Add user authentication callback and Prisma schema setup

--- app/(auth)/callback/page.tsx
+++ app/(auth)/callback/page.tsx
@@ -1,5 +1,21 @@
+import { onAuthenticateUser } from "@/actions/auth.actions";
+import { redirect } from "next/navigation";
+
 export const dynamic = "force-dynamic";
 
-const AuthCallbackPage = () => {};
+const AuthCallbackPage = async () => {
+  try {
+    const auth = await onAuthenticateUser();
+
+    if (auth.status === 200 || auth.status === 201) {
+      redirect("/home");
+    }
+
+    redirect("/");
+  } catch (error) {
+    console.error("Auth callback error:", error);
+    redirect("/");
+  }
+};
 
 export default AuthCallbackPage;
