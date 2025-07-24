efc55da02d0e2c8a61ef2b59f9d86c10310f22db
Samarth Kamble <kamblesamarth04@gmail.com>
2025-07-25 00:24:35 +0530

    refactor: Improve global Prisma client handling and clean up code

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prismaClient = global.prisma || new PrismaClient();
+export const prismaClient = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") {
-  global.prisma = prismaClient;
-}
+if (process.env.NODE_ENV !== "production")
+  globalForPrisma.prisma = prismaClient;
